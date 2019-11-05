3100b51d0889c2d8e1d09fa4a4f1990bc64c7177
Bastian Dolly Sapuhtra <43838336+basdos@users.noreply.github.com>
2019-11-05 09:47:22 +0700

    Add files via upload

---
+++ MainJava.java
@@ -0,0 +1,14 @@
+package pegadaiangit;
+
+public class MainJava {
+
+	public static void main(String[] args) {
+		// versi satu dulu yak
+		
+		System.out.println("Hallo bas");
+		System.out.println("Iya kenapa bas?");
+
+
+	}
+
+}
