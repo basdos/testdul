805ff0f2fcf283d922baad19ee7c6a21bc83a6e6
annisyaap <50549201+annisyaap@users.noreply.github.com>
2019-11-05 13:48:02 +0700

    Update MainJava.java

--- MainJava.java
+++ MainJava.java
@@ -8,6 +8,7 @@ public class MainJava {
 		System.out.println("Hallo bas (versi atu)");
 		System.out.println("Iya kenapa bas? (versi atu)");
 		System.out.println("aku mau bilang sesuatu nih (versi 2)");
+		System.out.println("Kamu mau bilang kalo kamu ga tahan jomblo ya?");
 
 
 	}
