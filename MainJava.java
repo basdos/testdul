5f9ac083b9a87689360e9513ce57769a08b31955
basdos <basdos7@gmail.com>
2019-11-05 10:03:22 +0700

    Update pertama nih dul
    apdet pertama bisa dul!

--- MainJava.java
+++ MainJava.java
@@ -5,8 +5,8 @@ public class MainJava {
 	public static void main(String[] args) {
 		// versi satu dulu yak
 		
-		System.out.println("Hallo bas");
-		System.out.println("Iya kenapa bas?");
+		System.out.println("Hallo bas (versi atu)");
+		System.out.println("Iya kenapa bas? (versi atu)");
 
 
 	}
